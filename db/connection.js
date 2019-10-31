7fa0d2ee2eb43e4b0154a97ec31b5ac3446bcfb7
Sandman105 <cpossandon@gmail.com>
2019-10-30 23:32:47 -0400

    updated skeleton files

--- db/connection.js
+++ db/connection.js
@@ -9,15 +9,18 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "", // add your local password here.
-    database: "" // add your db name here
+    password: "kershaw19", // add your local password here.
+    database: "notetaker_db" // add your db name here
   });
 }
 
+//What is this line of code doing?
 connection.config.typeCast = function(field, next) {
   if (field.type == "TINY" && field.length == 1) {
     return field.string() == "1"; // 1 = true, 0 = false
+    
   }
+  
   return next();
 };
 
