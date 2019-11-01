34d6078d04ed9c2d56bd90702fe025cc9697dc67
Sandman105 <cpossandon@gmail.com>
2019-10-31 23:49:27 -0400

    html routes finally working

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var path = require("path");
 var connection = require("./db/connection");
 //console.log(connection);
 
+// TODO: Import your route files from `route/`
+
 // Sets up the Express App
 // =============================================================
 //Again, this is where we have to initialize express.
@@ -16,10 +18,21 @@ var app = express();
 //This line allows you to deploy on localhost and if it's deployed on Heroku, so no changes need to be made.
 var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+console.log("Line 24");
+// TODO: Mount your HTML and API routes
+// https://expressjs.com/en/api.html#app.use
+var apiRoutes = require("./routes/apiRoutes");
+var htmlRoutes = require("./routes/htmlRoutes");
 
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
+
+
+//require("./routes/htmlRoutes")(app);
+//require("./routes/apiRoutes")(app);
 
 
 // Starts the server to begin listening and we know it's running from the console.log
